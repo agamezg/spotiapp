7b837d4b8a99cadd4feabaa6584c7e0d02e42a4e
ariel <ariel.gamez@redb.ee>
2020-10-04 16:15:38 -0300

    adding artist component

--- src/app/components/artist/artist.component.ts
+++ src/app/components/artist/artist.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute} from '@angular/router';
+import {SpotifyService} from '../../services/spotify.service';
 
 @Component({
   selector: 'app-artist',
@@ -7,11 +8,25 @@ import { ActivatedRoute} from '@angular/router';
   styleUrls: ['./artist.component.css']
 })
 export class ArtistComponent {
+  artist: any = {};
+  loading: boolean;
 
-  constructor(private router: ActivatedRoute) {
+  constructor(private router: ActivatedRoute,
+              private spotiService: SpotifyService) {
     this.router.params.subscribe(params => {
       console.log(`Receiving id: ${params.id} in artist component`);
+      this.getArtist(params.id);
     });
   }
 
+  getArtist(id: string): void {
+    this.loading = true;
+    this.spotiService.searchArtist(id)
+      .subscribe( artist => {
+        console.log('Artist founded');
+        console.log(artist);
+        this.artist = artist;
+        this.loading = false;
+      });
+  }
 }
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -14,7 +14,7 @@ export class SpotifyService {
   getQuery(query: string): any{
     const url = `https://api.spotify.com/v1/${query}`;
     const headers = new HttpHeaders({
-      Authorization: 'Bearer BQA5ccGxkHUmi-uk-bihXiSQsMdA-Coy14NTNYPAc6D2LbYz9NEoVqyYd0k0Vq7QFyPikeDIjNfpkpEifYc'
+      Authorization: 'Bearer BQDt20Eejsd3BHEv0JXbF-R2cVsODNH5WmwvvpAvUSGxsWaDqMPrwyk9ALhuhAc2HpGbdFLCW5AGhuz5FZQ'
     });
     return this.httpClient.get(url, {headers});
   }
@@ -27,5 +27,9 @@ export class SpotifyService {
   searchArtists(keyWord: string): any {
     return this.getQuery(`search?q=${keyWord}&type=artist&limit=15`)
       .pipe( map ((response: any) => response.artists.items));
+  }
+
+  searchArtist(id: string): any {
+    return this.getQuery(`artists/${id}`);
   }
 }
